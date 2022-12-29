7b12e1ad1b35c51e57004a464b1f2db18d7f4159
essAich-12 <saidhamzah12@gmail.com>
2022-12-29 10:16:05 +0700

    komit pertama

---
+++ login.php
@@ -0,0 +1,64 @@
+<?php
+require_once('./config/Login.php');
+
+if(!empty($_SESSION["id"])){
+  header("Location: index.php");
+}
+
+$login = new Login();
+
+if(isset($_POST["submit"])){
+  $result = $login->login($_POST["usernameemail"], $_POST["password"]);
+
+  if($result == 1){
+    $_SESSION["login"] = true;
+    $_SESSION["id"] = $login->idUser();
+    header("Location: index.php");
+  }
+  elseif($result == 10){
+    echo
+    "<script> alert('Password Salah'); </script>";
+  }
+  elseif($result == 100){
+    echo
+    "<script> alert('Pengguna tidak terdaftar'); </script>";
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/baru.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="form-wrapper sign-in">
+            <form action="" class="" method="POST" autocomplete="off">
+                <h2>Sign In</h2>
+                <div class="input-group">
+                    <input type="text" name="usernameemail" placeholder="Username Or Email" required value="" >
+                    <label for=""></label>
+                </div>
+                <div class="input-group">
+                    <input type="password" name="password" placeholder="Password" required value="">
+                    <label for=""></label>
+                </div>
+              
+                <button type="submit" name="submit">Sign In</button>
+                <div class="signUp-link">
+                    <p>Tidak Punya akun? <a href="register.php">Sign Up</a></p>
+                </div>
+              
+            </form>
+        </div>
+    </div>
+
+
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,79 @@
+<?php
+require_once('./config/Login.php');
+
+if(!empty($_SESSION["id"])){
+  header("Location: index.php");
+}
+
+$register = new Register();
+
+if(isset($_POST["submit"])){
+  $result = $register->registration($_POST["nama"], $_POST["username"], $_POST["email"], $_POST["password"], $_POST["confirmpassword"]);
+
+  if($result == 1){
+    echo
+    "<script> alert('Registrasi Berhasil'); </script>";
+  }
+  elseif($result == 10){
+    echo
+    "<script> alert('Username Atau Email Sudah Ada'); </script>";
+  }
+  elseif($result == 100){
+    echo
+    "<script> alert('Password Tidak Cocok'); </script>";
+  }
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!--  -->
+    <link rel="stylesheet" href="css/register1.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="form-wrapper sign-in">
+            <form action="" method="POST" autocomplete="off">
+                <h2 class="">Sign Up</h2>
+                <div class="input-group">
+                    <input id="satu" type="text" placeholder="Name" name="nama" required >
+                    <label for=""></label>
+                </div>
+                <div class="input-group">
+                    <input type="text" name="username"  placeholder="Username" required>
+                    <label for=""></label>
+                </div>
+                <div class="input-group">
+                    <input type="email" name="email" placeholder="Email" required>
+                    <label for=""></label>
+                </div>
+                <div class="input-group">
+                    <input type="password" name="password" placeholder="Password" required>
+                    <label for=""></label>
+                </div>
+                <div class="input-group ">
+                    <input type="password" name="confirmpassword" placeholder="Konfirmasi Password" required>
+                    <label for=""></label>
+                </div>
+               
+                <button class="id" type="submit" name="submit">Sign Up</button>
+                <div class="signUp-link">
+                    <p>Sudah Punya akun? <a href="login.php">Sign in</a></p>
+                </div>
+              
+            </form>
+        </div>
+    </div>
+
+
+    <script src="script.js"></script>
+</body>
+</html>
+
